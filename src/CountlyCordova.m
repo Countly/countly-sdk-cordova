6f9da9bc0809a50592968b0af98d4dbaf4fbdc47
nicolsondsouza <nicolsondsouza@gmail.com>
2014-10-29 09:58:41 +0530

    countly cordova added

---
+++ src/CountlyCordova.m
@@ -0,0 +1,57 @@
+/********* Echo.m Cordova Plugin Implementation *******/
+
+#import "Countly.h"
+#import "CountlyCordova.h"
+#import <Cordova/CDV.h>
+
+@implementation CountlyCordova
+
+- (void)echo:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+    
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)init:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* serverurl = [command.arguments objectAtIndex:0];
+    NSString* appkey = [command.arguments objectAtIndex:1];
+    NSString* myresult = @"ok";
+    
+    if (serverurl != nil && [serverurl length] > 0) {
+        [[Countly sharedInstance] start:appkey withHost:serverurl];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:myresult];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)event:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* serverurl = [command.arguments objectAtIndex:0];
+    NSString* appkey = [command.arguments objectAtIndex:1];
+    NSString* myresult = @"ok";
+    
+    if (serverurl != nil && [serverurl length] > 0) {
+        [[Countly sharedInstance] start:appkey withHost:serverurl];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"ok"];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
