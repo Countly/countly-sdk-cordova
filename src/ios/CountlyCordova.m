a58886708bc34a0592141887fb67ac846e699cce
nicolsondsouza <nicolsondsouza@gmail.com>
2014-12-15 15:53:36 +0530

    events added in ios

--- src/ios/CountlyCordova.m
+++ src/ios/CountlyCordova.m
@@ -1,4 +1,4 @@
-/********* Echo.m Cordova Plugin Implementation *******/
+/********* Countly.h Countly Plugin Implementation *******/
 
 #import "Countly.h"
 #import "CountlyCordova.h"
@@ -39,20 +39,53 @@
 - (void)event:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* key = [command.arguments objectAtIndex:0];
-    NSString* countString = [command.arguments objectAtIndex:1];
-    int countInt = [countString intValue];
-    if (key != nil && [key length] > 0) {
-//        if([key length] == 3){
-//            NSString* sumString = [command.arguments objectAtIndex:2];
-//            float sumFloat = [sumString floatValue];
-//            [[Countly sharedInstance] recordEvent:key count:countInt sum:sumFloat];
-//        }
-//        else{
-            [[Countly sharedInstance] recordEvent:key count:countInt];
-//        }
-        
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"event sent!"];
+    NSString* eventType = [command.arguments objectAtIndex:0];
+    if (eventType != nil && [eventType length] > 0) {
+        if ([eventType  isEqual: @"event"]) {
+            NSString* eventName = [command.arguments objectAtIndex:1];
+            NSString* countString = [command.arguments objectAtIndex:2];
+            int countInt = [countString intValue];
+            [[Countly sharedInstance] recordEvent:eventName count:countInt];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"event sent!"];
+        }
+        else if ([eventType  isEqual: @"eventWithSum"]){
+            NSString* eventName = [command.arguments objectAtIndex:1];
+            NSString* countString = [command.arguments objectAtIndex:2];
+            int countInt = [countString intValue];
+            NSString* sumString = [command.arguments objectAtIndex:3];
+            float sumFloat = [sumString floatValue];
+            [[Countly sharedInstance] recordEvent:eventName count:countInt  sum:sumFloat];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"eventWithSum sent!"];
+        }
+        else if ([eventType  isEqual: @"eventWithSegment"]){
+            NSString* eventName = [command.arguments objectAtIndex:1];
+            NSString* countString = [command.arguments objectAtIndex:2];
+            int countInt = [countString intValue];
+            NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
+            
+            for(int i=3,il=(int)command.arguments.count;i<il;i+=2){
+                dict[[command.arguments objectAtIndex:i]] = [command.arguments objectAtIndex:i+1];
+            }
+            [[Countly sharedInstance] recordEvent:eventName segmentation:dict count:countInt];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"eventWithSegment sent!"];
+        }
+        else if ([eventType  isEqual: @"eventWithSumSegment"]){
+            NSString* eventName = [command.arguments objectAtIndex:1];
+            NSString* countString = [command.arguments objectAtIndex:2];
+            int countInt = [countString intValue];
+            NSString* sumString = [command.arguments objectAtIndex:3];
+            float sumFloat = [sumString floatValue];
+            NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
+            
+            for(int i=4,il=(int)command.arguments.count;i<il;i+=2){
+                dict[[command.arguments objectAtIndex:i]] = [command.arguments objectAtIndex:i+1];
+            }
+            [[Countly sharedInstance] recordEvent:eventName segmentation:dict count:countInt  sum:sumFloat];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"eventWithSegment sent!"];
+        }
+        else{
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"none cases!"];
+        }
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
