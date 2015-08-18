9d86795ccbd3b6369f2a70b64b9f358138d81d56
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2015-08-18 18:26:32 +0530

    countly push code added

--- src/ios/CountlyCordova.m
+++ src/ios/CountlyCordova.m
@@ -132,8 +132,8 @@
     // NSString* token = [command.arguments objectAtIndex:0];
     // NSString* messagingMode = [command.arguments objectAtIndex:1];
     // int mode = [messagingMode intValue];
-    
-    // [[Countly sharedInstance] onRegistrationId:token withMode:mode];
+    // [[CountlyConnectionQueue sharedInstance] setStartedWithTest:YES];
+    // [[CountlyConnectionQueue sharedInstance] tokenSession:token];
     
     CDVPluginResult* pluginResult = nil;
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"onregistrationid!"];
