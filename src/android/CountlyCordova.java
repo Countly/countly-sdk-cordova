a457d75385b279881c2d7cd1f421f8c6f0355b68
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2017-02-11 16:49:42 +0530

    setOnce in progress

--- src/android/CountlyCordova.java
+++ src/android/CountlyCordova.java
@@ -24,22 +24,73 @@ public class CountlyCordova extends CordovaPlugin {
 
 			String serverUrl = args.getString(0);
 			String appKey = args.getString(1);
-			Countly.sharedInstance()
-				.init(context, serverUrl, appKey,null,DeviceId.Type.OPEN_UDID);
+
+			if(args.length() == 2){
+				Countly.sharedInstance()
+					.init(context, serverUrl, appKey,null,DeviceId.Type.OPEN_UDID);
+			}else if(args.length() == 3){
+				String yourDeviceID = args.getString(2);
+				Countly.sharedInstance()
+					.init(context, serverUrl, appKey,yourDeviceID,null);
+			}else if(){
+				Countly.sharedInstance()
+					.init(context, serverUrl, appKey,null,DeviceId.Type.ADVERTISING_ID);
+			}
+
 			Countly.sharedInstance().onStart(this.cordova.getActivity());
 			callbackContext.success("initialized!");
 			return true;
         }
+
+
+        else if ("changeDeviceId".equals(action)){
+        	String newDeviceID = args.getString(0);
+        	Countly.sharedInstance().changeDeviceId(newDeviceID);
+        	callbackContext.success("changeDeviceId success!");
+        }
+        else if ("setHttpPostForced".equals(action)){
+        	int isEnabled = Integer.parseInt(args.getString(0));
+        	if(isEnabled){
+        		Countly.sharedInstance().setHttpPostForced(true);
+        	}else{
+        		Countly.sharedInstance().setHttpPostForced(false);
+        	}
+        	callbackContext.success("setHttpPostForced success!");
+        }else if("enableParameterTamperingProtection".equals(action)){
+        	String salt = args.getString(0);
+        	Countly.sharedInstance().enableParameterTamperingProtection(salt);
+        	callbackContext.success("enableParameterTamperingProtection success!");
+        }else if("setLocation".equals(action)){
+        	double latitude = Integer.parseFloat(args.getString(0));
+        	double longitude = Integer.parseFloat(args.getString(1));
+        	Countly.sharedInstance().setLocation(latitude, longitude);
+        	callbackContext.success("setLocation success!");
+        }else if("enableCrashReporting".equals(action)){
+        	Countly.sharedInstance().enableCrashReporting();
+        	callbackContext.success("enableCrashReporting success!");
+        }else if("addCrashLog".equals(action)){
+        	String record = args.getString(0);
+        	Countly.sharedInstance().addCrashLog(String record);
+        	callbackContext.success("addCrashLog success!");
+        }
+
+
 		else if ("start".equals(action)) {
 			Countly.sharedInstance().onStart(this.cordova.getActivity());
             callbackContext.success("started!");
             return true;
-    }
+    	}
 		else if ("stop".equals(action)) {
 			Countly.sharedInstance().onStop();
             callbackContext.success("stoped!");
             return true;
-    }
+    	}else if("startEvent".equals(action)){
+    		String startEvent = args.getString(0);
+    		Countly.sharedInstance().startEvent(startEvent);
+    	}else if("endEvent".equals(action)){
+    		String endEvent = args.getString(0);
+    		Countly.sharedInstance().endEvent(endEvent);
+    	}
 		else if ("event".equals(action)) {
 			String eventType = args.getString(0);
 			if("event".equals(eventType)){
@@ -81,12 +132,14 @@ public class CountlyCordova extends CordovaPlugin {
 				callbackContext.success("event sent");
 			}
 			return true;
-    }
+    	}
 		else if ("setloggingenabled".equals(action)) {
 			Countly.sharedInstance().setLoggingEnabled(true);
 			callbackContext.success("setloggingenabled success!");
 			return true;
-    }
+    	}
+
+
 		else if ("setuserdata".equals(action)) {
 			// Bundle bundle = new Bundle();
 
@@ -109,7 +162,57 @@ public class CountlyCordova extends CordovaPlugin {
 			// Countly.sharedInstance().setUserData(bundle);
 			callbackContext.success("setuserdata success");
 			return true;
-    }
+    	}
+		else if ("userData.setProperty".equals(action)) {
+			String keyName = args.getString(0);
+			String keyValue = args.getString(1);
+			Countly.userData.setProperty(keyName, keyValue);
+			callbackContext.success("userData.setProperty success!");
+			return true;
+    	}
+		else if ("userData.increment".equals(action)) {
+			String keyName = args.getString(0);
+			Countly.userData.increment(keyName);
+			callbackContext.success("userData.increment success!");
+			return true;
+    	}
+		else if ("userData.incrementBy".equals(action)) {
+			String keyName = args.getString(0);
+			int keyIncrement = Integer.parseInt(args.getString(1));
+			Countly.userData.incrementBy(keyName, keyIncrement);
+			callbackContext.success("userData.incrementBy success!");
+			return true;
+    	}
+		else if ("userData.multiply".equals(action)) {
+			String keyName = args.getString(0);
+			int multiplyValue = Integer.parseInt(args.getString(1));
+			Countly.userData.multiply(keyName, multiplyValue);
+			callbackContext.success("userData.multiply success!");
+			return true;
+    	}
+		else if ("userData.saveMax".equals(action)) {
+			String keyName = args.getString(0);
+			int maxScore = Integer.parseInt(args.getString(1));
+			Countly.userData.saveMax(keyName, maxScore);
+			callbackContext.success("userData.saveMax success!");
+			return true;
+    	}
+		else if ("userData.saveMin".equals(action)) {
+			String keyName = args.getString(0);
+			int minScore = Integer.parseInt(args.getString(1));
+			Countly.userData.saveMin(keyName, minScore);
+			callbackContext.success("userData.saveMin success!");
+			return true;
+    	}
+		else if ("userData.setOnce".equals(action)) {
+			String keyName = args.getString(0);
+			String keyValue = args.getString(1);
+			Countly.userData.setOnce(keyName, minScore);
+			callbackContext.success("userData.setOnce success!");
+			return true;
+    	}
+
+
 		else if("onregistrationid".equals(action)){
 			int messagingMode = Integer.parseInt(args.getString(1));
 			Countly.CountlyMessagingMode mode = null;
