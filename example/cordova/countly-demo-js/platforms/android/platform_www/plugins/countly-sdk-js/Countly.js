3ef32de48c2c8c9b856e07b64c12b6f3c98459b7
ArtursK <kadikis.arturs@gmail.com>
2020-05-16 02:08:44 +0300

    Synchronising android sample app

--- example/cordova/countly-demo-js/platforms/android/platform_www/plugins/countly-sdk-js/Countly.js
+++ example/cordova/countly-demo-js/platforms/android/platform_www/plugins/countly-sdk-js/Countly.js
@@ -4,6 +4,7 @@ Countly.serverUrl = "";
 Countly.appKey = "";
 Countly.ready = false;
 Countly.version = "20.04";
+Countly.isDebug = false;
 var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 if (/android/i.test(userAgent)) {
     Countly.isAndroid = true;
@@ -32,8 +33,10 @@ Countly.recordView = function(recordView){
 };
 
 // countly enable logger
-Countly.setLoggingEnabled = function(){
-    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","setloggingenabled",[]);
+Countly.setLoggingEnabled = function(isDebug){
+    Countly.isDebug = isDebug;
+    var args = [isDebug?"true": "false"];
+    cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","setLoggingEnabled",args);
 }
 
 // countly sending user data
@@ -103,11 +106,16 @@ Countly.deviceready = function(){
 
 // countly dummy success and error event
 Countly.onSuccess = function(result){
-    // alert(result);
+    if(Countly.isDebug){
+        console.log("Countly.onSuccess");
+        console.log(result);
+    }
 }
 Countly.onError = function(error){
-     // alert("error");
-     // alert(error);
+    if(Countly.isDebug){
+        console.log("Countly.onError");
+        console.log(error);
+    }
 }
 
 // 2017
