92bffb1f30e206d8dcc298fea3fd2330eb531174
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2020-05-19 16:01:15 +0530

    Countly push test works all the time

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'countly:countly-sdk-js',
   version: '20.4.0', // Meteor doesn't go with 18.08.1
-  summary: 'Countly is an innovative, real-time, open source mobile analytics and push notifications platform.',
+  summary: 'Countly is an innovative, real-time, open source mobile analytics and push notifications platform. It collects data from mobile devices, and visualizes this information to analyze mobile application usage and end-user behavior. There are two parts of Countly: the server that collects and analyzes data, and mobile SDK that sends this data. Both parts are open source with different licensing terms.',
   git: 'https://github.com/Countly/countly-sdk-js.git',
   documentation: 'README.md'
 });
