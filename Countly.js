59f5c926552160d5d72a0ed1b73916796f394d1a
Muhammad Junaid Akram <mjunaid.akram@yahoo.com>
2020-09-21 11:19:30 +0500

    setLocation backward compatibility fixed.

--- Countly.js
+++ Countly.js
@@ -212,8 +212,9 @@ Countly.setLocationInit = function(countryCode, city, location, ipAddress){
     cordova.exec(Countly.onSuccess,Countly.onError,"CountlyCordova","setLocationInit",args);
 }
 
-Countly.setLocation = function(countryCode, city, location, ipAddress){
+Countly.setLocation = function(latitude, longitude, countryCode, city, ipAddress){
     var args = [];
+    var location  = latitude + " , " + longitude;
     args.push(countryCode || "null");
     args.push(city || "null");
     args.push(location || "null");
